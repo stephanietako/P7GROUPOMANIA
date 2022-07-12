0e4e53a57dfbf5519cc6004da1ceb0bd0b9fca99
stephanietako <gabeshine@live.fr>
2022-07-12 12:55:48 +0200

    connecting my database

--- backend/config/Database.js
+++ backend/config/Database.js
@@ -24,33 +24,4 @@ try {
 }
 
 export default db;
-// test1
-// const db = mysql.createPool({
-//     connectionLimit: 10,
-//     host: 'localhost',
-//     user: 'root',
-//     password: '',
-//     database: 'groupomania_db'
 
-// });
-
-//Get all users
-// app.get('', (req, res) => {
-//     db.getConnection((err, connection) => {
-//         if (err) throw err
-//         console.log('connected as id ' + connection.threadId)
-//         connection.query('SELECT * FROM `users`', (err, rows) => {
-//             connection.release() // return the connection to db
-
-//             if (!err) {
-//                 res.send(rows)
-//             } else {
-//                 console.log(err)
-//             }
-
-//             // if(err) throw err
-//             console.log('The data from user table are: \n', rows)
-//         })
-//     })
-// })
-//fin du test
