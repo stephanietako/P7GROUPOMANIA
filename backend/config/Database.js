1b5b2f8481723b7c099a46500b8e1b78522c0089
stephanietako <gabeshine@live.fr>
2022-07-21 00:30:01 +0200

    add env variable

--- backend/config/Database.js
+++ backend/config/Database.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize('groupomania_db', 'tako', 'tako22', {
+const db = new Sequelize('groupomania_db', process.env.DB_ACCESS_NAME, process.env.DB_ACCESS_PSWD, {
     host: 'localhost',
     dialect: 'mysql',
     port: 8889,
